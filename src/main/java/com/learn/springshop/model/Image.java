13967749506df83f1e35287c587e86628954baca
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-24 10:14:22 +0530

    Initial commit

---
+++ src/main/java/com/learn/springshop/model/Image.java
@@ -0,0 +1,4 @@
+package com.learn.springshop.model;
+
+public class Image {
+}
---
+++ src/main/java/com/learn/springshop/model/Product.java
@@ -0,0 +1,12 @@
+package com.learn.springshop.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int inventory;
+}
