d1107d82a696e712834f19725c139d3ae1eba052
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-24 15:09:46 +0530

    Product Service interface & Product Service build.

---
+++ src/main/java/com/learn/springshop/service/product/IProductService.java
@@ -0,0 +1,20 @@
+package com.learn.springshop.service.product;
+
+import com.learn.springshop.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+
+    Product addProduct(Product product);
+    Product getProductById(Long id);
+    void updateProduct(Product product);
+    void deleteProduct(Long id);
+    List<Product> getAllProducts();
+    List<Product> getProductsByName(String name);
+    List<Product> getProductsByBrand(Long id);
+    List<Product> getProductsByCategory(String category);
+    List<Product> getProductByBrandAndName(String brand, String name);
+    List<Product> getProductByCategoryAndBrand(String category, String brand);
+    Long countProductsByBrandAndName(String brand, String name);
+}
