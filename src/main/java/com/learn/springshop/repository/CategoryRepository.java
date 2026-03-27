c67a111ce96970edb959b3669f0d90cf5cffb8fe
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-27 11:35:36 +0530

    category repository file done.

---
+++ src/main/java/com/learn/springshop/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.learn.springshop.repository;
+
+import com.learn.springshop.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
+}
